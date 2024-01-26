21820e464b2aadf08f48d063ddc15a9b05aaf549
7moqasim7 <144913171+7moqasim7@users.noreply.github.com>
2024-01-26 16:28:30 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Besteloverzicht</title>
+    <link rel="stylesheet" href="syles.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Besteloverzicht</h1>
+        <p><a href="index.html"> naar het formulier</a></p>
+    </div>
+</body>
+</html>
+
+
